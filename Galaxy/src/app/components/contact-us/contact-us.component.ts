e84d7add3d465bac24a72a016597689d3fc535b1
shorouk-soliman <shoroukabdelhamied4@gmail.com>
2024-06-24 12:26:38 +0300

    added about and contact us

--- Galaxy/src/app/components/contact-us/contact-us.component.ts
+++ Galaxy/src/app/components/contact-us/contact-us.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
-  styleUrl: './contact-us.component.css'
+  styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
+  submissionSuccess: boolean = false;
+  submissionError: boolean = false;
 
+  onSubmit(form: any) {
+    if (form.valid) {
+      this.submissionSuccess = true;
+      this.submissionError = false;
+      form.resetForm(); 
+    } else {
+      this.submissionError = true;
+      this.submissionSuccess = false;
+    }
+  }
 }
