49064ab1aa02b0acd219f342bd4c3c399d1dd91f
shorouk-soliman <shoroukabdelhamied4@gmail.com>
2024-06-27 17:49:31 +0300

    added notification for user and password

--- Galaxy/src/app/components/default/user/user-password-settings/user-password-settings.component.ts
+++ Galaxy/src/app/components/default/user/user-password-settings/user-password-settings.component.ts
@@ -1,39 +1,52 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { UnitService } from '../../../../services/unit.service';
+import { ConfirmMessageComponent } from '../../../shared-componentes/confirm-message/confirm-message.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-password-settings',
   templateUrl: './user-password-settings.component.html',
-  styleUrl: './user-password-settings.component.css'
+  styleUrls: ['./user-password-settings.component.css']
 })
 export class UserPasswordSettingsComponent {
-  constructor(private unit:UnitService){}
+  notificationMessage: string | null = null;
 
+  constructor(private unit: UnitService, public dialog: MatDialog) {}
 
-  @Output() ToggleForm:EventEmitter<any> = new EventEmitter<any>();
-  @Output() refreshUser:EventEmitter<any> = new EventEmitter<any>()
+  @Output() ToggleForm: EventEmitter<any> = new EventEmitter<any>();
+  @Output() refreshUser: EventEmitter<any> = new EventEmitter<any>();
 
-
-  Toggle(){
-    this.ToggleForm.emit(); 
+  Toggle() {
+    this.ToggleForm.emit();
   }
 
-
   PasswordForm: FormGroup = new FormGroup({
-    oldpassword: new FormControl('',[ Validators.required,Validators.minLength(8)]),
-    newpassword: new FormControl('',[ Validators.required,Validators.minLength(8)]),
+    oldpassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    newpassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
   });
 
+  OldError = () => this.PasswordForm.get('oldpassword')?.errors;
+  NewError = () => this.PasswordForm.get('newpassword')?.errors;
 
-  OldError = () => this.PasswordForm.get('oldpassword')?.errors
-  NewError = () => this.PasswordForm.get('newpassword')?.errors
-
-
-  onSubmit(){
-    if(!this.PasswordForm.valid) return;
-    this.unit.user.UpdatePassword(this.PasswordForm.value).subscribe((res)=>{
+  onSubmit() {
+    if (!this.PasswordForm.valid) return;
+    this.unit.user.UpdatePassword(this.PasswordForm.value).subscribe((res) => {
+      this.notificationMessage = "ok! It changed successfully";
       this.refreshUser.emit();
+    });
+  }
+
+  confirmUpdateuser(): void {
+    const dialogRef = this.dialog.open(ConfirmMessageComponent, {
+      data: { message: 'Are you sure you want change the password؟' },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.onSubmit();
+      } else {
+        this.notificationMessage = "cancelled";
+      }
     });
   }
 }
